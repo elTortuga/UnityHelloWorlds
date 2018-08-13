2adc2eda873ea1fd65d4169ccca662c3da00c14f
elTortuga <mds831@gmail.com>
2018-08-13 09:57:05 -0700

    Example of game run through

---
+++ Assets/GameRunThrough/_Scripts/MenuSettingsSceneManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuSettingsSceneManager : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is calle once per frame
+    void Update()
+    {
+
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
---
+++ Assets/GameRunThrough/_Scripts/SplashManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class SplashManager : MonoBehaviour
+{
+    public string timeTillLoad;
+    public Text timeTillLoadGuiText;
+
+    private bool stuffIsLoaded;
+    // Use this for initialization
+    void Start()
+    {
+        this.stuffIsLoaded = false;
+        StartCoroutine(DoBuisness());
+    }
+
+    IEnumerator DoBuisness()
+    {
+        yield return new WaitForSeconds(5);
+        this.stuffIsLoaded = true;
+        this.timeTillLoadGuiText.text = (5 - Time.timeSinceLevelLoad).ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.stuffIsLoaded)
+        {
+			MoveOnToNextScene();
+        }
+    }
+
+
+
+    void MoveOnToNextScene()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+
+    }
+}
